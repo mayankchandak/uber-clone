323e34e7f291737f309a3aa11e85d904d406e703
mayankchandak <60479264+mayankchandak@users.noreply.github.com>
2022-08-24 23:58:57 +0530

    Implement Keyboard avoiding view

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 import { Provider } from 'react-redux'
 import HomeScreen from './screens/HomeScreen';
 import MapScreen from './screens/MapScreen';
@@ -8,6 +8,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context'
 import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
+import tw from 'twrnc'
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -16,6 +17,11 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
       <SafeAreaProvider>
+        <KeyboardAvoidingView 
+        behavior={Platform.OS === 'ios' ? 'padding':'height'}
+        style={{flex:1}}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+        >
         <Stack.Navigator>
           <Stack.Screen 
             name='HomeScreen' 
@@ -32,17 +38,9 @@ export default function App() {
             }}
           />
         </Stack.Navigator>
+        </KeyboardAvoidingView>
       </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
