d2ee5e26bf3b0db5248fd4f0db6aeed8911088d1
mayankchandak <60479264+mayankchandak@users.noreply.github.com>
2022-08-25 08:43:59 +0530

    Build NavigateCard component

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -8,6 +8,8 @@ import { setDestination } from '../slices/navSlice'
 import { useNavigation } from '@react-navigation/native'
 import RideOptionsCard from './RideOptionsCard'
 import NavFavourites from './NavFavourites'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import {Icon} from "react-native-elements"
 
 
 const NavigateCard = () => {
@@ -40,8 +42,21 @@ const NavigateCard = () => {
                 nearbyPlacesAPI="GooglePlacesSearch"
                 debounce={400}
                 />
+                <NavFavourites />
             </View>
-            <NavFavourites />
+            <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
+                 <TouchableOpacity 
+                 onPress={() => navigation.navigate("RideOptionsCard")}
+                 style={tw`flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}>
+                    <Icon name="car" type="font-awesome" color="white" size={16} />
+                    <Text style={tw`text-white text-center`}>Rides</Text>
+
+                 </TouchableOpacity>
+                 <TouchableOpacity style={tw`flex flex-row justify-between w-24 px-4 py-3`}>
+                    <Icon name="fast-food-outline" type="ionicon" color="black" size={16}/>
+                    <Text style={tw`text-center`}>Eats</Text>
+                 </TouchableOpacity>
+            </View>
         </SafeAreaView>
   )
 }
